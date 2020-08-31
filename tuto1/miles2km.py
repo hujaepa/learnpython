f8ca96873f1baf96b94f4e17529bd8b84e9272b3
hujaepa <hujaepa@gmail.com>
2020-08-31 21:30:53 +0800

    update comment in miles2km.py

--- tuto1/miles2km.py
+++ tuto1/miles2km.py
@@ -1,7 +1,13 @@
 """
 This program is to convert miles into km.
 type casting is needed when receiving input as string in order to convert into number.
-Vice versa
+Vice versa.
+Standard Data type in python:
+    -Numbers
+    -String
+    -List
+    -Tuple
+    -Dictionary
 """
 
 miles = input("Enter how many miles: ")
